01a94114420052b2ad72d0768d4ee7fee3288434
Dexeraz <50703222+Dexeraz@users.noreply.github.com>
2023-02-21 21:13:52 +0100

    UserList component added. Fething data when adding user to the list

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
-import styles from './AddUser.module.css'
+import styles from "./AddUser.module.css";
 import Button from "../UI/Button";
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
+  //enteredUsername - latest state snapshot;
+  //setEnteredusername reload component
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0){
+        return;
+    }
+    if (+enteredAge<1){ //plus before enteredAge ensures that it is a number (we input string)
+        return;
+    }
+    props.onAddUser(enteredUsername, enteredAge);
+    setEnteredUsername("");
+    setEnteredAge("");
+  };
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
   };
 
   //We added Card's 'className=' property inside of Card.js file so we can input a style
@@ -13,9 +36,14 @@ const AddUser = (props) => {
     <Card className={styles.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text"></input>
+        <input
+          id="username"
+          type="text"
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+        ></input>
         <label htmlFor="age">Age (years)</label>
-        <input id="age" type="number"></input>
+        <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler}></input>
         <Button type="submit">Add User</Button>
       </form>
     </Card>
